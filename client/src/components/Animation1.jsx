3f8cac72990d01eb45f1742b665462af42828e46
Raja543 <rajakumarmahto952@gmail.com>
2023-04-02 01:40:00 +0530

    adding some files

---
+++ client/src/components/Animation1.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Lottie from "react-lottie";
+import animationData from "./Animations/65396-secure-blockchain.json";
+
+const Animation1 = () => {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+
+  return (
+    <div>
+      <Lottie options={defaultOptions} height={550} width={550} />
+    </div>
+  );
+};
+
+export default Animation1;
--- client/src/components/Herofirst.js
+++ client/src/components/Herofirst.js
@@ -6,25 +6,76 @@ import Card from "./Profilecard";
 import Discordsvg from "./Discordsvg";
 import Twittersvg from "./Twittersvg";
 import Instagramsvg from "./Instagramsvg";
+import Animation1 from "./Animation1";
 
 const Herofirst = () => {
   return (
     <>
       <div className="hero">
-        <h1 className="hero-heading">
-          Revolutionize Your File Storage with Decentralization
-        </h1>
-        <h1 className="hero-subheading"> Your Data, Your Control</h1>
-        <p className="hero__description">
-          Join the movement towards a more decentralized web, where users own
-          and control their data, and where censorship and surveillance are a
-          thing of the past.
+        <div className="bg-animate">
+          <h1 className="hero-heading">
+            Revolutionize Your File Storage with Decentralization
+          </h1>
+          <h1 className="hero-subheading"> Your Data, Your Control</h1>
+          <p className="hero__description">
+            Join the movement towards a more decentralized web, where users own
+            and control their data, and where censorship and surveillance are a
+            thing of the past.
+          </p>
+          <button className="hero-button">
+            <Link className="hero-button-text" to="/secondpage">
+              Click here to Upload the file
+            </Link>
+          </button>
+        </div>
+      </div>
+      {/* aninate section */}
+      <div className="animate-section">
+        <h2 className="animate-heading">What is SecureShareX ?</h2>
+        <p className="animate-para">
+          SecureShareX is building the next generation of infrastructure for
+          Web3 interoperability, scalability, and privacy through advanced
+          Zero-knowledge proof (ZKP) technology. Polyhedra Network provides
+          trust-minimized and highly-efficient interoperability solutions for
+          asset transfers, message passing, and data sharing between various
+          Web2 and Web3 systems.
         </p>
-        <button className="service-button">
-          <Link className="service-button-text" to="/secondpage">
-            Click here to Upload the file
-          </Link>
-        </button>
+      </div>
+
+      {/* Product section */}
+      <div className="product-section">
+        <div className="right-image">
+          <Animation1 />
+        </div>
+        <h2 className="product-head">ZK Full Stack for web3</h2>
+        <div className="product-detail">
+          <div className="Para-1">
+            <h3 className="para1-heading">Interoperability</h3>
+            <p className="para1-detail">
+              Polyhedra Network implements infrastructures for Web3
+              interoperability across heterogeneous systems which guarantee
+              strong security without extra trust assumptions.
+            </p>
+          </div>
+          <hr className="my-hr" />
+          <div className="Para-2">
+            <h3 className="para2-heading">Scalability</h3>
+            <p className="para2-detail">
+              Polyhedra Network designs and develops scalable and efficient
+              zero-knowledge proof protocols for large-scale ZKP systems with
+              distributed computing networks.
+            </p>
+          </div>
+          <hr className="my-hr" />
+          <div className="Para-3">
+            <h3 className="para3-heading">Fine-grained Privacy</h3>
+            <p className="para3-detail">
+              Polyhedra Network provides fine-grained privacy extensions for
+              Web3 with stealth address models, meeting both privacy-preserving
+              needs and compliance policies.
+            </p>
+          </div>
+        </div>
       </div>
 
       {/* About section */}
