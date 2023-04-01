4cbc44b37a2e5c03003f67f7ec24dc0607b60cb4
Dipk47 <88235474+Dipk47@users.noreply.github.com>
2023-04-02 01:45:00 +0530

    secondpage_changes

--- client/src/components/Secondpage.js
+++ client/src/components/Secondpage.js
@@ -57,44 +57,17 @@ const Secondpage = () => {
         <Navbar />
       </div>
 
-      {/* Main service section */}
-      <div className="account-container">
-        <p className="account-">
-          Account : {account ? account : "Not connected"}
-        </p>
-      </div>
+      
       <div className="file-container">
-        <h4 className="file-heading">Image</h4>
-        <p className="image-description">
-          File types supported : JPG,PNG,SVG,GIF,MP4. Max size : 10MB
-        </p>
-        <div className="image-container">
-          <p className="image-description"> drop your images here or browse</p>
-          <FileUpload
-            account={account}
-            provider={provider}
-            contract={contract}
-          ></FileUpload>
-        </div>
-        {/* share button */}
-        {!modalOpen && (
-          <button className="share-button" onClick={() => setModalOpen(true)}>
-            Share
-          </button>
-        )}
-        {modalOpen && (
-          <Modal setModalOpen={setModalOpen} contract={contract}></Modal>
-        )}
-
-        {/* upload button */}
-        {/* <button type="submit" className="upload-button " disabled={!file}>
-          Upload File
-        </button> */}
+        <button className="upload-file-button"></button>
       </div>
 
-      {/* File Check Section */}
-      <div className="">
-        <Display account={account} contract={contract}></Display>
+      <div className="brief-detail">
+        <h1>How The Image Uploading Works</h1>
+      </div>
+
+      <div className="upload-check-sectionS">
+        
       </div>
 
       <div className="footer-section">
