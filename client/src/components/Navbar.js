91fea690d038d6d30cf8cacbabc7227fa2405bf3
Dipk47 <88235474+Dipk47@users.noreply.github.com>
2023-04-18 00:51:24 +0530

    navbar system set

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -144,6 +144,8 @@
 
 import React, { useState } from "react";
 import "./Navbar.css";
+import { NavLink } from "react-router-dom";
+import Connectwallet from "./Connectwallet";
 
 function Navbar() {
   const [showMenu, setShowMenu] = useState(false);
@@ -154,22 +156,33 @@ function Navbar() {
 
   return (
     <nav className="navbar">
-      <div className="navbar-logo">Logo</div>
+      <div className="navbar-logo" >SecureShareX</div>
       <div className={`navbar-menu ${showMenu ? "show" : ""}`}>
         <ul>
-          <li>
-            <a href="#">Home</a>
-          </li>
-          <li>
-            <a href="#">About</a>
-          </li>
-          <li>
-            <a href="#">Services</a>
-          </li>
-          <li>
-            <a href="#">Contact</a>
-          </li>
+        <li className="nav-item">
+           <NavLink className="nav-link space" to="/">
+             Home
+           </NavLink>
+         </li>
+        <li className="nav-item">
+           <NavLink className="nav-link space" to="/about">
+             Working
+           </NavLink>
+         </li>
+         <li className="nav-item">
+           <NavLink className="nav-link space" to="/accesslist">
+             Allowlist
+           </NavLink>
+         </li>
+         <li className="nav-item">
+           <NavLink className="nav-link space" to="/contact">
+             Contact
+           </NavLink>
+         </li>
         </ul>
+        <li className="wallet-display">
+        <Connectwallet />
+       </li>
       </div>
       <div className="navbar-toggle" onClick={handleMenuClick}>
         <div className="toggle-line"></div>
