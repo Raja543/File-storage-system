0a12fa96fb4b6492b9bb602eeb75bcb489496763
Raja <rajakumarmahto952@gmail.com>
2023-05-03 19:33:27 +0530

    Responsive #9

--- client/src/components/Firstpage.js
+++ client/src/components/Firstpage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Firstpage.css";
 import "./Secondpage";
@@ -6,9 +6,18 @@ import Discordsvg from "./Discordsvg";
 import Twittersvg from "./Twittersvg";
 import Instagramsvg from "./Instagramsvg";
 import myvideo from "./videos/first.mp4";
+import img1 from "./images/first-image.png";
+import img2 from "./images/second-image.png";
+import img3 from "./images/third-image.png";
 import Navbar from "./Navbar";
 
-const Firstpage = () => {
+function Firstpage() {
+  const [imageSrc, setImageSrc] = useState(img3);
+
+  const handleHeadingClick = (newImageSrc) => {
+    setImageSrc(newImageSrc);
+  };
+
   return (
     <div>
       <div className="navbar-section">
@@ -61,43 +70,59 @@ const Firstpage = () => {
 
       {/* Product section */}
       <div className="product-section">
-        <h2 className="product-head">
-          Benefits of Blockchain-based File Storage
-        </h2>
+        <h2 className="product-head">Benefits of Blockchain-based File Storage</h2>
         <div className="right-image">
-          
+          <img src={imageSrc} alt="logo" />
         </div>
         <div className="product-detail">
           <div className="Para-1">
-            <h3 className="para1-heading">Immutability</h3>
+            <h3
+              id="para1-heading"
+              className="para1-heading"
+              onClick={() => handleHeadingClick(img2)}
+            >
+              Immutability
+            </h3>
             <p className="para1-detail">
-              The blockchain provides a tamper-proof and immutable record of the
-              file. Once a file is stored on the blockchain, it cannot be
-              altered or deleted, ensuring the integrity of the data.
+              The blockchain provides a tamper-proof and immutable record of the file.
+              Once a file is stored on the blockchain, it cannot be altered or deleted,
+              ensuring the integrity of the data.
             </p>
           </div>
           <hr className="my-hr" />
           <div className="Para-2">
-            <h3 className="para2-heading">Decentralization</h3>
+            <h3
+              id="para2-heading"
+              className="para2-heading"
+              onClick={() => handleHeadingClick(img3)}
+            >
+              Decentralization
+            </h3>
             <p className="para2-detail">
-              The file is stored on a decentralized network of computers,
-              meaning that there is no single point of failure or control. This
-              ensures that the file remains accessible and secure even if one or
-              more nodes fail.
+              The file is stored on a decentralized network of computers, meaning that
+              there is no single point of failure or control. This ensures that the file
+              remains accessible and secure even if one or more nodes fail.
             </p>
           </div>
           <hr className="my-hr" />
           <div className="Para-3">
-            <h3 className="para3-heading">Transparency</h3>
+            <h3
+              id="para3-heading"
+              className="para3-heading"
+              onClick={() => handleHeadingClick(img1)}
+            >
+              Transparency
+            </h3>
             <p className="para3-detail">
-              All transactions on the blockchain are transparent and publicly
-              visible. This means that anyone can view the transaction history
-              of the file and verify its authenticity, providing a high level of
-              trust and accountability.
+              All transactions on the blockchain are transparent and publicly visible.
+              This means that anyone can view the transaction history of the file and
+              verify its authenticity, providing a high level of trust and accountability.
             </p>
           </div>
         </div>
       </div>
+
+
 
       {/* About section */}
       <div className="about">
@@ -113,118 +138,6 @@ const Firstpage = () => {
           ensure that your data is encrypted, tamper-proof, and always
           available, even in the face of network outages or attacks.
         </p>
-
-        {/* profile card */}
-        {/* <div className="profile-body">
-          <div className="photo">
-            <img
-              src="https://content-static.upwork.com/uploads/2014/10/01073427/profilephoto1.jpg"
-              className="image--cover"
-            />
-          </div>
-          <div className="profile">
-            <div id="rating-stars" align="center">
-              <div className="stars">
-                <label
-                  aria-label="first-star"
-                  className="star-label"
-                  htmlFor="rate-1"
-                >
-                  <i className="rate-icon star-icon fa fa-star"></i>
-                </label>
-                <input
-                  className="rate-input"
-                  name="rating"
-                  id="rate-1"
-                  value="1"
-                  type="radio"
-                />
-                <label
-                  aria-label="second-star"
-                  className="star-label"
-                  htmlFor="rate-2"
-                >
-                  <i className="rate-icon star-icon fa fa-star"></i>
-                </label>
-                <input
-                  className="rate-input"
-                  name="rating"
-                  id="rate-2"
-                  value="2"
-                  type="radio"
-                />
-                <label
-                  aria-label="third-star"
-                  className="star-label"
-                  htmlFor="rate-3"
-                >
-                  <i className="rate-icon star-icon fa fa-star"></i>
-                </label>
-                <input
-                  className="rate-input"
-                  name="rating"
-                  id="rate-3"
-                  value="3"
-                  type="radio"
-                />
-                <label
-                  aria-label="fourth-star"
-                  className="star-label"
-                  htmlFor="rate-4"
-                >
-                  <i className="rate-icon star-icon fa fa-star"></i>
-                </label>
-                <input
-                  className="rate-input"
-                  name="rating"
-                  id="rate-4"
-                  value="4"
-                  type="radio"
-                />
-                <label
-                  aria-label="fifth-star"
-                  className="star-label"
-                  htmlFor="rate-5"
-                >
-                  <i className="rate-icon star-icon fa fa-star"></i>
-                </label>
-                <input
-                  className="rate-input"
-                  name="rating"
-                  id="rate-5"
-                  value="5"
-                  type="radio"
-                />
-              </div>
-            </div>
-            <h1 className="username">Raja Kumar</h1>
-            <h2 className="profession"> Developer </h2>
-            <h3 className="locationname">
-              <i className="fas fa-map-marker-alt"></i>New Delhi , India
-            </h3>
-            <p className="descricao">
-              Mussum Ipsum, cacilds vidis litro abertis. Tá deprimidis, eu
-              conheço uma cachacis que pode alegrar sua vidis. Quem manda na
-              minha terra sou euzis! Nullam volutpat risus nec leo commodo, ut
-              interdum diam laoreet. Sed non consequat odio. Suco de cevadiss, é
-              um leite divinis, qui tem lupuliz, matis, aguis e fermentis.
-            </p>
-            <div className="socialmedia">
-              <a href="#" className="button-facebook">
-                <i className="fab fa-facebook-square"></i>
-              </a>
-              <a href="#" className="button-instagram">
-                <i className="fab fa-instagram"></i>
-              </a>
-              <a href="#" className="button-twitter">
-                <i className="fab fa-twitter-square"></i>
-              </a>
-              <a href="#" className="button-whatsapp">
-                <i className="fab fa-whatsapp-square"></i>
-              </a>
-            </div>
-          </div>
-        </div> */}
       </div>
 
       {/* footer-section */}
@@ -254,6 +167,6 @@ const Firstpage = () => {
       </div>
     </div>
   );
-};
+}
 
 export default Firstpage;
