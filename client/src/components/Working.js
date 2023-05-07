242321c406d2a64997bd2e298624e4f21e0fa20b
Raja <rajakumarmahto952@gmail.com>
2023-05-08 02:18:10 +0530

    adding working page

--- client/src/components/Working.js
+++ client/src/components/Working.js
@@ -12,8 +12,8 @@ const Working = () => {
             <div className="navbar-section">
                 <Navbar />
             </div>
+            <h1 className="Work-heading">Working of File Storage System</h1>
             <div className="first-section">
-                <h1 className="Work-heading">Working of File Storage System</h1>
                 <p className="first-para">
                     The process of storing an image on IPFS through Pinata involves three main steps.
                     The first step is to upload the image file to the Pinata platform.This can be done
@@ -24,23 +24,23 @@ const Working = () => {
                     After the image is pinned, Pinata generates a hash value for the file. This hash value
                     is a unique identifier that can be used to access the file on the IPFS network.
                 </p>
-                <div className="right-image">
-                    <img src={image1} alt="first-image" />
+                <div className="image">
+                    <img src={image1}  alt="first-image" />
                 </div>
             </div>
             <div className="second-section"></div>
-            <p className="first-para">
+            <p className="second-para">
                 The second step is to store the hash on the blockchain, along with any other relevant
                 metadata. This allows the user to associate the image with a specific blockchain
                 transaction, making it easier to verify the authenticity and ownership of the image.
                 The hash can be stored on a public blockchain, such as Ethereum, or on a private
                 blockchain, depending on the user's needs.
             </p>
-            <div className="right-image">
-                <img src={image2} alt="first-image" />
+            <div className="image">
+                <img src={image2} alt="second-image" />
             </div>
             <div className="third-section"></div>
-            <p className="first-para">
+            <p className="third-para">
                 The final step is to access the image on the IPFS network. To do this, the user simply needs to
                 provide the hash value to an IPFS client. This client can be a web browser with IPFS support, a
                 command-line tool, or a software application that integrates with IPFS. Once the client receives
@@ -50,7 +50,7 @@ const Working = () => {
                 a permanent, reliable, and secure way to store and access files, with the added benefits of
                 decentralization and blockchain integration.
             </p>
-            <div className="right-image">
+            <div className="image">
                 <img src={image3} alt="first-image"></img>
             </div>
         </div>
