0a9f593277e4c22829c559e0f8138f769d04f6f5
Dipk47 <88235474+Dipk47@users.noreply.github.com>
2023-05-14 10:11:24 +0530

    upload file type issue solved

--- client/src/components/FileUpload.js
+++ client/src/components/FileUpload.js
@@ -54,8 +54,6 @@ const FileUpload = ({ contract, account, provider }) => {
     setUploadProgress(0);
   };
 
-
-
   const retrieveFile = (e) => {
     const input = e.target;
     const label = document.getElementById("fileLabel");
@@ -70,7 +68,66 @@ const FileUpload = ({ contract, account, provider }) => {
     }
   };
 
-
+  const renderPreview = () => {
+    if (file) {
+      const fileType = file.type.split("/")[0];
+  
+      switch (fileType) {
+        case "image":
+          return (
+            <img
+              src={URL.createObjectURL(file)}
+              alt="Uploaded file"
+              className="preview-image"
+            />
+          );
+        case "video":
+          return (
+            <video
+              src={URL.createObjectURL(file)}
+              className="preview-video"
+              controls
+              height={150}
+              width={320}
+            />
+          );
+        case "audio":
+          return (
+            <audio
+              src={URL.createObjectURL(file)}
+              className="preview-audio"
+              controls
+            />
+          );
+        case "application":
+          if (file.type === "application/pdf") {
+            return (
+              <div className="preview-file">
+                <i className="fa-solid fa-file-pdf"></i>
+                <p>PDF FILE</p>
+              </div>
+            );
+          } else {
+            return (
+              <div className="preview-file">
+                <i className="fa-solid fa-file"></i>
+                <p>{fileType.toUpperCase()} FILE</p>
+              </div>
+            );
+          }
+        default:
+          return (
+            <div className="preview-file">
+              <i className="fa-solid fa-file"></i>
+              <p>{fileType.toUpperCase()} FILE</p>
+            </div>
+          );
+      }
+    }
+  
+    return null;
+  };
+  
 
   const [currentButton, setCurrentButton] = useState("upload");
 
@@ -87,31 +144,61 @@ const FileUpload = ({ contract, account, provider }) => {
       <div className="upload-share-container">
         <div className="toggleWrapper">
           <input type="checkbox" className="dn" id="dn" />
-          <label htmlFor="dn" className="toggle" onClick={() => setCurrentButton((currentButton === "share") ? "upload" : "share")}>
+          <label
+            htmlFor="dn"
+            className="toggle"
+            onClick={() =>
+              setCurrentButton(currentButton === "share" ? "upload" : "share")
+            }
+          >
             <span className="toggle__handler"></span>
           </label>
         </div>
         {currentButton === "upload" && (
           <div className="wrapper">
             <h3>Upload Your image</h3>
-            <p className="first-desc">File supported type : PNG , JPEG , JPG , WEBP{" "}</p>
-            <div className="form" onClick={() => document.getElementById("my-file").click()} onSubmit={handleSubmit}>
-              <label htmlFor="my-file" id="fileLabel" className="custom-file-upload">
+            <p className="first-desc">
+              File supported type : PNG , JPEG , JPG , WEBP{" "}
+            </p>
+            <div
+              className="form"
+              onClick={() => document.getElementById("my-file").click()}
+              onSubmit={handleSubmit}
+            >
+              <label
+                htmlFor="my-file"
+                id="fileLabel"
+                className="custom-file-upload"
+              >
                 <i className="fa-solid fa-cloud-arrow-up fa-bounce"></i>
               </label>
-              <input type="file" id="my-file" name="myfile" disabled={!account} onChange={(e) => {
-                retrieveFile(e);
-                document.getElementById("upload-para").style.display = "none";
-              }} />
-              {file && (
-                <img src={URL.createObjectURL(file)} alt="Uploaded file" className="preview-image" />
-              )}
-              <p id="upload-para" className="upload-para">Browse or Drag here to upload</p>
+              <input
+                type="file"
+                id="my-file"
+                name="myfile"
+                disabled={!account}
+                onChange={(e) => {
+                  retrieveFile(e);
+                  document.getElementById("upload-para").style.display = "none";
+                }}
+              />
+              {file && renderPreview()}
+
+              <p id="upload-para" className="upload-para">
+                Browse or Drag here to upload
+              </p>
               {uploadProgress > 0 && (
                 <progress value={uploadProgress} max={100} />
               )}
             </div>
-            <button type="submit" className="upload" disabled={!file} onClick={handleSubmit}>Upload</button>
+            <button
+              type="submit"
+              className="upload"
+              disabled={!file}
+              onClick={handleSubmit}
+            >
+              Upload
+            </button>
           </div>
         )}
         {currentButton === "share" && (
@@ -122,7 +209,6 @@ const FileUpload = ({ contract, account, provider }) => {
         )}
       </div>
     </>
-
   );
 };
 export default FileUpload;
