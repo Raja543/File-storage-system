929c46b2e025c79962af9c9f82c4913dda763060
Dipk47 <88235474+Dipk47@users.noreply.github.com>
2023-06-19 09:57:23 +0530

    hardhat config

--- client/src/components/Secondpage.js
+++ client/src/components/Secondpage.js
@@ -43,7 +43,7 @@ const Secondpage = () => {
             const signer = provider.getSigner();
             const address = await signer.getAddress();
             setAccount(address);
-            let contractAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+            let contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
             const contract = new ethers.Contract(contractAddress, Upload.abi, signer);
             setContract(contract);
             setProvider(provider);
