42b795ddb4a7f2f04fa371a6f34396304f6cd1fc
Raja Kumar <rajakumarmahto952@gmail.com>
2024-02-04 18:16:09 +0530

    feature section revamp

--- client/src/components/FeatureCard.jsx
+++ client/src/components/FeatureCard.jsx
@@ -10,12 +10,12 @@ const FeatureCard = ({ image, title, description }) => {
 
   return (
     <div
-      className="flex flex-col p-6 bg-[#050606] text-[#fff] rounded-lg"
+      className="flex flex-col p-6 bg-[#121916] text-[#fff] rounded-lg"
       data-aos="fade-right"
       data-aos-offset="300"
       data-aos-easing="ease-in-sine"
     >
-      <img className="h-14 w-20 p-2" src={image} alt={title} />
+      <img className="h-14 w-14 p-2" src={image} alt={title} />
       <h3 className="text-lg font-bold py-1 text-[#e3e3e3e3]">{title}</h3>
       <p className="text-left text-sm py-1 text-gray-400">{description}</p>
     </div>
--- client/src/components/Features.jsx
+++ client/src/components/Features.jsx
@@ -9,37 +9,37 @@ const featureData = [
     title: "File Upload",
     description:
       "Securely upload files to the blockchain, ensuring tamper-proof storage. This feature guarantees the integrity and security of user data, providing a robust foundation for file storage.",
-    image: "",
+    image: "/icons/upload.png",
   },
   {
     title: "File Sharing",
     description:
       "Facilitate collaboration by sharing files with other users through the allowlist. By adding specific addresses, users grant controlled access to shared content, fostering efficient and secure information exchange.",
-    image: "",
+    image: "/icons/upload.png",
   },
   {
     title: "Direct Browser Access",
     description:
       "Enjoy seamless file access and viewing directly through a web browser. When connected to the same wallet, this convenient feature enhances user accessibility and streamlines the file viewing experience.",
-    image: "",
+    image: "/icons/upload.png",
   },
   {
     title: "Allowlist Management",
     description:
       "Take charge of shared file accessibility with ease. Users can effortlessly manage the allowlist by adding or removing addresses, maintaining complete control over who can view their shared files.",
-    image: "",
+    image: "/icons/upload.png",
   },
   {
     title: "Privacy and Security",
     description:
       "Ensure utmost privacy and security. Users can disallow specific addresses from accessing their files, reinforcing control over shared content and safeguarding sensitive information.",
-    image: "",
+    image: "/icons/upload.png",
   },
   {
     title: "File Deletion",
     description:
       "Users can now delete their uploaded files, offering an additional layer of control and privacy. This feature empowers users to manage their stored content efficiently, ensuring a secure and customizable user experience.",
-    image: "",
+    image: "/icons/upload.png",
   },
 ];
 
@@ -50,7 +50,7 @@ const Features = () => {
   return (
     <div
       className="bg-cover bg-center px-16 py-20 text-[#fff] bg-[#000] "
-      style={{ backgroundImage: `url('/Specialimages/landing-center.png')` }}
+      style={{ backgroundImage: `url('/images/landing-center.png')` }}
     >
       <h1
         className="text-xl md:text-2xl lg:text-4xl font-bold py-4"
